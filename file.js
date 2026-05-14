cb735a62ed5fbcb6fe4e3d50bb2dbccb7c96efa3
ManavGaur27 <manavgaur270@gmail.com>
2026-05-14 16:51:20 +0530

    portfoloi is updated

---
+++ file.js
@@ -0,0 +1,7 @@
+const product = [
+  {id: 101, name: "laptop", price: 1000, category: "tech" }, 
+  {id: 102, name: "Mouse", category: "tech", instock: "true" },
+  {id: 103, name: "Monitor", price: 300, category: "tech", instock : "false" },
+]
+
+
